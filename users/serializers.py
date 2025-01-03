275e5a4780c90261e093eb5381c16885a1a4c7bf
K-MarkLee <seungyeol6825@gmail.com>
2025-01-03 16:20:40 +0900

    delete: other apps

--- users/serializers.py
+++ users/serializers.py
@@ -1,5 +1,15 @@
 from rest_framework import serializers
-from .models import users
+from datetime import datetime
+from django.contrib.auth.models import get_user_model
+from django.contrib.auth.password_validation import validate_password
 
 
+User = get_user_model()
 
+class UserCreateSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password]) #검증 
+    password2 = serializers.CharField(write_only=True, requried=True) # 확인이라서 검증 필요없음
+
+    class Meta:
+        model = User # User모델
+        fields = ['email', 'username', '']
--- users/views.py
+++ users/views.py
@@ -1,5 +1,21 @@
+from rest_framework import status
 from django.shortcuts import render
+from rest_framework.response import Response
+from .serializers import UserCreateSerializer
+from django.contrib.auth import get_user_model
+from rest_framework.decorators import api_view, permission_classes, authentication_classes
+
+User = get_user_model()
+
 
 # Create your views here.
-def login(request):
-    return render(request, "users/login.html")
+@api_view(['POST']) # POST 요청만 받음
+@permission_classes([])
+@authentication_classes([])
+
+def create_user(request):
+    serializer = UserCreateSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response({"message" : "회원가입이 완료되었습니다!"}, status=status.HTTP_201_CREATED)
+    return render(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
