4654b5b4538a282f3b868586fa3cd60d3d480466
K-MarkLee <seungyeol6825@gmail.com>
2025-01-08 00:58:05 +0900

    MDD-22 update : todo > urls, views, models, serializers / need test

--- todo/models.py
+++ todo/models.py
@@ -2,23 +2,11 @@ from django.db import models
 from django.conf import settings 
 
 class Todo(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE) #유저 모델
-    content = models.CharField(max_length=100) #To-Do 할 일 작성
-    select_date = models.DateField() # 입력한 날짜
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE) #유저 모델 연결
+    content = models.CharField(max_length=100) #To-Do 할 일
+    is_completed = models.BooleanField(default=False) # 완료 여부
+    select_date = models.DateField() # 할일 의 날짜
     created_at = models.DateTimeField(auto_now_add=True) # 생성일자
 
-    
-    
     def __str__(self):
-        return self.content
-    
-class CheckList(models.Model):
-    todo = models.ForeignKey(Todo, on_delete=models.CASCADE, related_name='checklists')  # Todo와 연결
-    content = models.TextField()  # 체크리스트 내용
-    checked = models.BooleanField(default=False)  # 완료 여부
-
-
-def __str__(self):
-    return f"{self.content} - {'Checked' if self.checked else 'Unchecked'}"
-
-    
+        return f"{self.content} - {'Completed' if self.is_completed else 'Incomplete'}" # 할일 - 완료 여부
--- todo/views.py
+++ todo/views.py
@@ -1,9 +1,9 @@
-from .models import Todo
+from .models import Todo, Checklist
+from datetime import datetime
 from rest_framework import status
-from .serializers import TodoSerializer, CheckListSerializer
+from .serializers import TodoSerializer, ChecklistSerializer
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from datetime import datetime
 
 
 # To-do list 할일 표시
@@ -14,7 +14,7 @@ def todo_list(request, date): # 달력으로 인해 특정 날짜에 관해서 t
         selected_date = datetime.strptime(date, "%Y-%m-%d").date()
 
     except ValueError:
-        return Response({"error": "Invalid date format. Use YYYY-MM-DD."}, status=status.HTTP_400_BAD_REQUEST)
+        return Response({"error": "잘못된 입력입니다. yyyy-mm-dd 형식으로 입력해주세요. "}, status=status.HTTP_400_BAD_REQUEST)
 
     # 해당 날짜에 해당하는 To-do 필터링
     todo = Todo.objects.filter(user=request.user, select_date=selected_date).order_by('-created_at')
@@ -35,23 +35,14 @@ def todo_create(request):
 
 
 
-# 할 일 수정/삭제
-@api_view(['PUT','DELETE'])
+# 할 일 삭제
+@api_view(['DELETE'])
 def todo_update(request, todo_id):
     try:
-        todo = Todo.objects.get(pk=todo_id, user=request.user) # 우저의 할 일만 수정 가능하게
+        todo = Todo.objects.get(todo_id=todo_id, user=request.user) # 우저의 할 일만 수정 가능하게
     except Todo.DoesNotExist:
         return Response({"error": "Todo를 찾을 수 없습니다."}, status=status.HTTP_404_NOT_FOUND)
-        
-    if request.method == 'PUT':
-        serializer = TodoSerializer(todo, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-    if request.method == 'DELETE':
-        todo.delete()
-        return Response({"message": "Todo를 성공적으로 삭제했습니다."}, status=status.HTTP_200_OK)
-
+    
+    todo.delete()
+    return Response({"message": "Todo를 성공적으로 삭제했습니다."}, status=status.HTTP_200_OK)
+    
