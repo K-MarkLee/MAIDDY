256d0ae3fae40989af7d93f300a1a649226f3a58
K-MarkLee <seungyeol6825@gmail.com>
2025-01-08 22:08:01 +0900

    MDD-18 fix : diaries > urls,views - combined update and create function

--- diaries/urls.py
+++ diaries/urls.py
@@ -5,9 +5,9 @@ from . import views
 app_name = 'diaries'
 
 urlpatterns = [
-    path('detail/', views.diary_detail, name='diary-detail'),  # 다이어리  조회 (GET) (api/diaries/detail/?date=2025-01-01)
-    path('create/', views.diary_create_and_update, name='diary-create'),  # 다이어리 생성
-    path('update/', views.diary_update, name='diary-update'),  # 다이어리 수정 
+    path('', views.diary_detail, name='diary-detail'),  # 다이어리  조회 (GET) (api/diaries/detail/?date=2025-01-01)
+    path('update/', views.diary_update, name='diary-update'),  # 다이어리 생성
+
 ]
 
 
--- diaries/views.py
+++ diaries/views.py
@@ -30,46 +30,31 @@ def diary_detail(request):
 
 # 다이어리 작성 
 @api_view(['POST'])
-def diary_create_and_update(request): 
+def diary_update(request): # 생성 및 수정
     date = request.data.get('select_date') # 날짜를 받아옴
-    if Diary.objects.filter(select_date=date, user=request.user).exists(): # 날짜가 이미 존재하는 경우
-        return Response({"error": "이미 작성된 다이어리가 있습니다."}, status=status.HTTP_400_BAD_REQUEST)
-    
-    serializer = DiarySerializer(data=request.data) # 다이어리 생성을 위한 직렬화
-    if serializer.is_valid(): # 직렬화된 데이터가 유효한지 확인
-        # 다이어리 저장 시 현재 사용자 정보와 연결
-        new_diary = serializer.save(user = request.user) # 저장된 다이어리 객체를 diary 변수에 할당
-        return Response(DiarySerializer(new_diary).data, status=status.HTTP_201_CREATED) # 저장된 데이터 반환
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) # 오류 발생시 오류 반환
-
-
-
-
-
-# 다이어리 수정
-@api_view(['PUT','PATCH'])
-def diary_update(request):
-    date = request.query_params.get('date')
     if not date: # 날짜가 없을 경우
         return Response({"error": "날짜가 필요합니다. YYYY-MM-DD 형식으로 전달해주세요."}, status=status.HTTP_400_BAD_REQUEST)
+    
     try:
         select_date = datetime.strptime(date, "%Y-%m-%d").date() # 날짜 형식 변환
     except ValueError:
         return Response({"error": "날짜 형식이 올바르지 않습니다. YYYY-MM-DD 형식으로 전달해주세요."}, status=status.HTTP_400_BAD_REQUEST)
     
+
     try:
         diary = Diary.objects.get(select_date = select_date, user=request.user)  # 사용자별로 다이어리 조회
-    except Diary.DoesNotExist:
-        return Response({"error": "다이어리를 찾을 수 없습니다."}, status=status.HTTP_404_NOT_FOUND)
-    
+        serializer = DiarySerializer(diary, data = request.data, partial=True) # 직렬화
+        if serializer.is_valid():
+            update_diary = serializer.save()  # 다이어리 수정
+            return Response(DiarySerializer(update_diary).data, status=status.HTTP_200_OK) # 수정된 데이터 반환
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) # 오류 발생시 오류 반환
 
-    #다이어리 수정
-    partial_update = request.method == 'PATCH'
-    serializer = DiarySerializer(diary, data=request.data, partial=partial_update)
+    except Diary.DoesNotExist: # 다이어리가 없을 경우
 
-    if serializer.is_valid():
-        update_diary = serializer.save()  # 다이어리 수정
+        serializer = DiarySerializer(data=request.data) # 다이어리 생성을 위한 직렬화
 
-        return Response(DiarySerializer(update_diary).data, status=status.HTTP_200_OK)
-    
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        if serializer.is_valid(): # 직렬화된 데이터가 유효한지 확인
+            # 다이어리 저장 시 현재 사용자 정보와 연결
+            new_diary = serializer.save(user = request.user)
+            return Response(DiarySerializer(new_diary).data, status=status.HTTP_201_CREATED) # 저장된 데이터 반환
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) # 오류 발생시 오류 반환
