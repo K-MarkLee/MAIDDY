3e237e4d2815dcb68b654941fa17a7bc1964612c
K-MarkLee <seungyeol6825@gmail.com>
2025-01-08 11:47:41 +0900

    MDD-22 fix : todo urls

--- todo/urls.py
+++ todo/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'todo'
 
 urlpatterns = [
-    path('todo/<str:date>/', views.todo_list, name='todo'),  # 할 일 목록 표시 (GET)
+    path('todo/<str:date>/', views.todo_list, name='todo-list'),  # 할 일 목록 표시 (GET)
     path('create/', views.todo_create, name = 'todo-create'), # 할 일 생성 (POST)
     path('delete/<int:todo_id>', views.todo_delete, name='todo-delete'),  # 할 일 삭제 (DELETE)
 ]
