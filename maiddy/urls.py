1b807baf3145c52ff3a1db1ec106a2a846b6c182
K-MarkLee <seungyeol6825@gmail.com>
2025-01-29 18:32:10 +0900

    fix : delete merge cache

--- maiddy/urls.py
+++ maiddy/urls.py
@@ -4,17 +4,12 @@ from .views import ChatbotAPIView, FeedbackAPIView, RecommendAPIView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-<<<<<<< HEAD
-]
-=======
     path("api/users/", include('users.urls')),
     path("api/diaries/", include('diaries.urls')),
     path("api/todo/", include('todo.urls')),
     path("api/schedules/", include('schedules.urls')),
   
-  
     path('ai/chatbot/', ChatbotAPIView.as_view(), name='chatbot'),
     path('ai/feedback/', FeedbackAPIView.as_view(), name='feedback'),
     path('ai/recommend/', RecommendAPIView.as_view(), name='recommend'),
 ]
->>>>>>> mvp
